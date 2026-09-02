a99fc1c54c8c3f460394a1821b7d2aa07c683740
Succh <91399298+Succh@users.noreply.github.com>
2026-09-02 18:05:03 +0800

    fix: 补 size 扩展函数 import

--- app/src/main/java/com/succh/unifeed/ui/AddFeedDialog.kt
+++ app/src/main/java/com/succh/unifeed/ui/AddFeedDialog.kt
@@ -1,6 +1,7 @@
 package com.succh.unifeed.ui
 
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
