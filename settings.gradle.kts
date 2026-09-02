bff3923278125c2365001fcf5590c387075b9181
Succh <91399298+Succh@users.noreply.github.com>
2026-09-02 17:27:19 +0800

    fix: remove RepositoriesMode reference (avoid unresolved reference in settings script)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,6 @@ pluginManagement {
 }
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
