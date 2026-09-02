b698ee7324025825c827bc072e4bb68238d52459
Succh <91399298+Succh@users.noreply.github.com>
2026-09-02 17:26:44 +0800

    fix: correct dependencyResolution to dependencyResolutionManagement (Kotlin DSL syntax)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,12 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolution {
+dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
